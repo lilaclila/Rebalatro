26d72f68ce644ad7e864a8882992e25e65dbb299
lilaclila <thrasherglitches@gmail.com>
2026-06-21 19:06:44 +0100

    Delete Rebalatro directory

--- Rebalatro/src/blinds.lua
+++
@@ -1,9 +0,0 @@
-SMODS.Blind:take_ownership('bl_serpent', {
-	loc_txt = {
-    	name = 'The Serpent ',
-	text = {
-	"After Play or Discard",
-	"draw at most 3 cards",
-	 }
-	},
-})
--- Rebalatro/src/planets.lua
+++
@@ -1,3 +0,0 @@
-SMODS.PokerHand:take_ownership('Flush',          { l_chips = 20 }, true)
-SMODS.PokerHand:take_ownership('Full House',     { l_chips = 30 }, true)
-SMODS.PokerHand:take_ownership('Five of a Kind', { l_chips = 40 }, true)
